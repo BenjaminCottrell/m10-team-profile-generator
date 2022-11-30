b67bf98c8e23518be99960cf3e20d5a7e040a39b
BenCottrell <bencottrell48@yahoo.com>
2022-11-29 23:33:17 -0500

    edited variable naming scheme of Engineer tests to be more easily readable

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,9 +2,9 @@ const Engineer = require("../lib/Engineer");
 
 // tests for Engineer need to have the github username
 test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
-  const newEngineer = new Engineer("Guy", 1, "test@email.com", testValue);
-  expect(newEngineer.github).toBe(testValue);
+  const githubUsername = "GitHubUser";
+  const newEngineer = new Engineer("Guy", 1, "test@email.com", githubUsername);
+  expect(newEngineer.github).toBe(githubUsername);
 });
 
 // Engineer needs to have name, id, email and github
@@ -15,7 +15,7 @@ test("getRole() should return \"Engineer\"", () => {
 });
 
 test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
-  const newEngineer = new Engineer("Guy", 1, "test@email.com", testValue);
-  expect(newEngineer.getGithub()).toBe(testValue);
+  const role = "GitHubUser";
+  const newEngineer = new Engineer("Guy", 1, "test@email.com", role);
+  expect(newEngineer.getGithub()).toBe(role);
 });
