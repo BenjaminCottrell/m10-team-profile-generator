e785e07a64d27effce7559413c3a4c04141bb208
BenCottrell <bencottrell48@yahoo.com>
2022-11-30 00:57:49 -0500

    added html structure in template literal

--- index.js
+++ index.js
@@ -16,79 +16,82 @@ const numValidation = (name) => {
   let valid = +name
   return ((valid.toString() !== 'NaN') ? true : 'Not a number. Try again.')
 }
+
 // function for creating manager - inquirer questions
 function beginQuestions() {
-  inquirer.prompt( 
- [
-  {
-    name: 'role',
-    type: 'list',
-    message: 'Which type of Employee do you want to add?',
-    choices: ['Manager', 'Engineer', 'Intern'],
-  },
-  {
-    name: 'name',
-    type: 'input',
-    message: 'What is the employees name?',
-  },
-  {
-    name: 'id',
-    type: 'input',
-    message: 'What is the employee ID number?',
-    validate: numValidation,
-  },
-  {
-    name: 'email',
-    type: 'input',
-    message: 'What is their email?',
-  }
-])
-  // function to use what role has been chosen to determine what additional info the user should input
-  .then(function({name, role, id, email}) {
-    let roleExtraInfo = "";
-    if (role === "Engineer") {
-      roleExtraInfo = "Github username";
-    } else if (role === "Intern") {
-      roleExtraInfo = "school name";
-    } else {
-      roleExtraInfo = "Office Number"
-    }
-    inquirer.prompt([{
-      message: `Enter the team member's ${roleExtraInfo}`,
-      name: "roleExtraInfo"
-  },
-  {
-      type: "list",
-      message: "Do you want to add more team members?",
-      choices: [
+  inquirer.prompt(
+    [
+      {
+        name: 'role',
+        type: 'list',
+        message: 'Which type of Employee do you want to add?',
+        choices: ['Manager', 'Engineer', 'Intern'],
+      },
+      {
+        name: 'name',
+        type: 'input',
+        message: 'What is the employees name?',
+      },
+      {
+        name: 'id',
+        type: 'input',
+        message: 'What is the employee ID number?',
+        validate: numValidation,
+      },
+      {
+        name: 'email',
+        type: 'input',
+        message: 'What is their email?',
+      }
+    ])
+    // function to use what role has been chosen to determine what additional info the user should input
+    .then(function ({ name, role, id, email }) {
+      let roleExtraInfo = "";
+      if (role === "Engineer") {
+        roleExtraInfo = "Github username";
+      } else if (role === "Intern") {
+        roleExtraInfo = "school name";
+      } else {
+        roleExtraInfo = "Office Number"
+      }
+      inquirer.prompt([{
+        message: `Enter the team member's ${roleExtraInfo}`,
+        name: "roleExtraInfo"
+      },
+      {
+        type: "list",
+        message: "Do you want to add more team members?",
+        choices: [
           "yes",
           "no"
-      ],
-      name: "addMembers"
-    }])
-    // function to add new members to the team
-    .then(function ({roleExtraInfo, addMembers}) {
-      let newMember;
-      if (role === "Engineer") {
-          newMember = new Engineer(name, id, email, roleExtraInfo);
-      } else if (role === "Intern") {
-          newMember = new Intern(name, id, email, roleExtraInfo);
-      } else {
-          newMember = new Manager(name, id, email, roleExtraInfo);
-      }
-      // push new member to the teamMembers array
-      teamMembers.push(newMember);
-      addHtml(newMember)
-      .then(function() {
-          if (addMembers === "yes") {
-              beginQuestions();
+        ],
+        name: "addMembers"
+      }])
+        // function to add new members to the team
+        .then(function ({ roleExtraInfo, addMembers }) {
+          let newMember;
+          if (role === "Engineer") {
+            newMember = new Engineer(name, id, email, roleExtraInfo);
+          } else if (role === "Intern") {
+            newMember = new Intern(name, id, email, roleExtraInfo);
           } else {
-              finishHtml();
+            newMember = new Manager(name, id, email, roleExtraInfo);
           }
+          // push new member to the teamMembers array
+          teamMembers.push(newMember);
+          addHtml(newMember)
+            .then(function () {
+              if (addMembers === "yes") {
+                beginQuestions();
+              } else {
+                finishHtml();
+              }
+            })
+        })
     })
-  })
-  })
 }
+
+
 
 
 // STRUCTURING IT
--- src/page-template.js
+++ src/page-template.js
@@ -1,24 +1,70 @@
+function startHTML(teamMembers) {
+    const html =
+        // using template literal with HTML formatting
+    `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <script src="https://cdn.tailwindcss.com"></script>
+          <title>Team Profile Generator</title>
+    </head>
+    
+    <body class="flex flex-col h-screen justify-between">
+    
+        <header class="p-8 bg-rose-700 mb-6">
+            <h1 class="text-white text-center text-5xl"><a href="./test.html"
+                    class="hover:text-slate-300 ease-in-out duration-300"><i class="fa-solid fa-people-group"></i></a> Team Profile Generator
+            </h1>
+        </header>
+        
+        <main>
+            <section id="aside-top" class="flex flex-wrap m-2 gap-auto justify-around"></section>
+        </main>
+        
+        <footer class="bg-rose-700 grid grid-cols-1 justify-items-center mt-6 pb-1">
+            <nav class="grid grid-cols-3 justify-items-center max-w-7xl w-10/12">
+                <div class="py-3 text-center">
+                    <a href="https://github.com/BenjaminCottrell" target="_blank" rel="noopener noreferrer"
+                        class="ease-in-out duration-300 text-white hover:text-slate-300 px-2 py-2 rounded-md text-sm font-medium">GitHub</a>
+                </div>
+                <div class="py-3 text-center">
+                    <a href="https://benjamincottrell.github.io/ben-cottrell-webdev-portfolio//" target="_blank"
+                        rel="noopener noreferrer"
+                        class="ease-in-out duration-300 text-white hover:text-slate-300 px-2 py-2 rounded-md text-sm font-medium">Portfolio</a>
+                </div>
+            </nav>
+        </footer>
+    </body>
+    
+    </html>
+    `;
+
+  }
+
 const generateTeam = team => {
 
     // create the manager html
     const generateManager = manager => {
         return `
-        
-        `;
+
+            `;
     };
 
     // create the html for engineers
     const generateEngineer = engineer => {
         return `
 
-        `;
+            `;
     };
 
     // create the html for interns
     const generateIntern = intern => {
         return `
 
-        `;
+            `;
     };
 
     // use the team array to generate pieces of html based on the employee role
